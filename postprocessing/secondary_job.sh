f2649f468f2e20dd815616ca7aecc0c5852f3b36
Stuart Grieve <s.grieve@qmul.ac.uk>
2019-10-22 20:54:59 +0100

    parallelising the job while the scheduler is down

--- postprocessing/secondary_job.sh
+++ postprocessing/secondary_job.sh
@@ -7,11 +7,19 @@
 #$ -pe smp 1
 #$ -l node_type=nxv
 #$ -l h_vmem=4G
-#$ -l h_rt=10:0:0
+#$ -l h_rt=1:0:0
+#$ -t 1-13
+#$ -tc 13
 
 module load python/3.6.3
 
 source /data/home/faw513/toku-env/bin/activate
 
-# Run the application.
-python /data/home/faw513/gdbm/postprocessing/secondary_analysis.py
+# Parse parameter file to get variables.
+number=$SGE_TASK_ID
+paramfile=/data/home/faw513/gdbm/postprocessing/params.txt
+
+index=`sed -n ${number}p $paramfile | awk '{print $1}'`
+variable1=`sed -n ${number}p $paramfile | awk '{print $2}'`
+
+python /data/home/faw513/gdbm/postprocessing/secondary_analysis.py $variable1
