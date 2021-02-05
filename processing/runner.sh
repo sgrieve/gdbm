bfbb552af5355d84028b2166052680a529eeaa82
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-02-05 14:13:03 +0000

    replacing hgt with tif in the runner script

--- processing/runner.sh
+++ processing/runner.sh
@@ -16,7 +16,7 @@ cd $1
 python /data/home/faw513/gdbm/processing/get_urls.py $1.shp | xargs -n 1 -P 8 -I FILEPATH /data/home/faw513/aws/v2/current/bin/aws s3 cp FILEPATH . --endpoint-url https://opentopography.s3.sdsc.edu --no-sign-request
 
 # Build virtual raster from tiles
-gdalbuildvrt input.vrt *.hgt
+gdalbuildvrt input.vrt *.tif
 
 # Clip the merged raster using the corresponding shapefile
 gdalwarp -multi -wo 'NUM_THREADS=val/ALL_CPUS' -srcnodata -32768 -dstnodata -9999 -cutline /data/home/faw513/gdbm/climate_zones/singlepart_files_split/$1.shp -crop_to_cutline -of ENVI input.vrt tmp.bil
@@ -26,7 +26,7 @@ gdalwarp -t_srs '+proj=utm +zone='$2' +datum=WGS84 +'$3'' -of ENVI -ot Float32 t
 
 # Tidy up some temp files
 rm tmp.*
-rm *.hgt
+rm *.tif
 rm *.vrt
 
 # Run the LSD code (on legion)
