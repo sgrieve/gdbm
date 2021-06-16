e7ac40d3ded09c8c3625d904379fcf19f61e3c77
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-06-16 13:58:44 +0100

    adding the pit fill calculations to the secondary analysis code

--- postprocessing/secondary_analysis.py
+++ postprocessing/secondary_analysis.py
@@ -17,7 +17,7 @@ zone = sys.argv[1]
 output_filename = '/data/Geog-c2s2/gdbm/{}_data'.format(zone)
 
 # Store the output strings as a list, where the first item is a header
-output = ['RiverName,NCI,Relief,FlowLength,TotalSlope,Area,ai_mean,ai_median,ai_std,ai_min,ai_max,ai_n\n']
+output = ['RiverName,NCI,Relief,FlowLength,TotalSlope,Area,ai_mean,ai_median,ai_std,ai_min,ai_max,ai_n,pit_pixel_proportion,pit_lenth_proportion\n']
 
 # Get the list of files to be processed
 final_file_list = glob('/data/Geog-c2s2/gdbm-complete/*/{}*river*.csv'.format(zone))
@@ -51,19 +51,36 @@ for filename in final_file_list:
     Y = m * A + b  # Y values on the line
     offset_elev = (B - Y) / R
 
-    # Add the results to out list of results
+    # Add the results to our list of results
     offset_elevs.append(offset_elev)
     NCI = np.nanmedian(offset_elev)
+
+    flowdir = data[:, 8]
+    pit_flags = data[:, 10]
+    pixel_pit_prop = np.sum(pit_flags) / len(pit_flags)
+
+    # Get the flowdirections of all pixels that have been filled
+    pit_flowdirs = flowdir[np.where(pit_flags == 1)]
+
+    # If the flowdir is cardinal, set it to root(2)*datares, otherwise set it to datares
+    # Data is all 30 m resolution
+    pit_lengths = np.where(pit_flowdirs % 2 == 1, np.sqrt(2) * 30, 30)
+
+    # Sum up all these lengths to get the total length of the channel impacted by pit filling
+    # and divide by total channel lenth
+    length_pit_prop = np.sum(pit_lengths) / FlowLength
 
     # Also need the total river length, river relief, river slope and name
     river_name = os.path.splitext(os.path.basename(filename))[0]
 
-    output.append('{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(river_name, NCI, R,
+    output.append('{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(river_name, NCI, R,
                                                                 FlowLength, R / FlowLength, area,
                                                                 np.nanmean(AI), np.nanmedian(AI),
                                                                 np.nanstd(AI), np.nanmin(AI),
                                                                 np.nanmax(AI),
-                                                                np.count_nonzero(~np.isnan(AI))
+                                                                np.count_nonzero(~np.isnan(AI)),
+                                                                pixel_pit_prop,
+                                                                length_pit_prop
                                                                 ))
 
 with open('{}.csv'.format(output_filename), 'w') as f:
