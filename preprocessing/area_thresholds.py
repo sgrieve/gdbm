84ff35a00984668690e9fa5da181bd9334f1139b
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-06-17 18:00:24 +0100

    there are some climate tiles that do not intersect with srtm coverage. These are ignored in later processing, but we need to ignore them here too.

--- preprocessing/area_thresholds.py
+++ preprocessing/area_thresholds.py
@@ -7,18 +7,21 @@ from math import ceil
 res_data = {}
 
 for file in glob('../res_jsons/*.json'):
-    with open(file) as f:
-        data = json.loads(f.read())
+    try:
+        with open(file) as f:
+            data = json.loads(f.read())
 
-    river_id = os.path.splitext(os.path.basename(file))[0]
+        river_id = os.path.splitext(os.path.basename(file))[0]
 
-    resolution = data['geoTransform'][1]
-    cell_area = resolution ** 2
+        resolution = data['geoTransform'][1]
+        cell_area = resolution ** 2
 
-    # This is the channel threshold, min basin area, max basin area
-    res_data[river_id] = [ceil(22500000 / cell_area),
-                          ceil(45000000 / cell_area),
-                          ceil(450000000 / cell_area)]
+        # This is the channel threshold, min basin area, max basin area
+        res_data[river_id] = [ceil(22500000 / cell_area),
+                              ceil(45000000 / cell_area),
+                              ceil(450000000 / cell_area)]
+    except:
+        pass
 
 with open('../processing/threshold_areas.json', 'w') as f:
     json.dump(res_data, f, sort_keys=True, indent=4)
