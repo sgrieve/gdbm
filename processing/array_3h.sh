abd1a6010a4c7afa34303621baec7f4c59540d51
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-07-19 15:46:21 +0100

    setting up the job scripts for the low and high sensi analysis

--- processing/array_3h.sh
+++ processing/array_3h.sh
@@ -2,13 +2,13 @@
 
 #$ -cwd
 #$ -j y
-#$ -N gdbm-1
-#$ -o /data/Geog-c2s2/gdbm/
+#$ -N gdbm-sensi-3h
+#$ -o /data/Geog-c2s2/gdbm-sensi-h/
 #$ -pe smp 1
 #$ -l node_type=nxv
-#$ -l h_vmem=64G
-#$ -l h_rt=1:0:0
-#$ -t 1-174
+#$ -l h_vmem=128G
+#$ -l h_rt=4:0:0
+#$ -t 1-619:3
 #$ -tc 100
 
 module load gdal/2.3.1
@@ -21,7 +21,7 @@ source /data/home/faw513/toku-env/bin/activate
 
 # Parse parameter file to get variables.
 number=$SGE_TASK_ID
-paramfile=/data/home/faw513/gdbm/processing/array_params_0_1.txt
+paramfile=/data/home/faw513/gdbm/processing/array_params_15_99.txt
 
 index=`sed -n ${number}p $paramfile | awk '{print $1}'`
 variable1=`sed -n ${number}p $paramfile | awk '{print $2}'`
