cc190106a2866d2611946ae02904ad8723b77ba8
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-07-19 16:06:43 +0100

    updating the path for the secondary analysis to reflect the location of the fully re-run code

--- postprocessing/secondary_analysis.py
+++ postprocessing/secondary_analysis.py
@@ -20,7 +20,7 @@ output_filename = '/data/Geog-c2s2/gdbm/{}_data'.format(zone)
 output = ['RiverName,NCI,Relief,FlowLength,TotalSlope,Area,ai_mean,ai_median,ai_std,ai_min,ai_max,ai_n,pit_pixel_proportion,pit_lenth_proportion,straightness_proportion\n']
 
 # Get the list of files to be processed
-final_file_list = glob('/data/Geog-c2s2/gdbm-complete/*/{}*river*.csv'.format(zone))
+final_file_list = glob('/data/Geog-c2s2/gdbm/*/{}*river*.csv'.format(zone))
 
 # Cycle through every river file in our list and get the metrics.
 for filename in final_file_list:
