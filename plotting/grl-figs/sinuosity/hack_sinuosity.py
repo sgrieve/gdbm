b957cc0058965b6cb0fc031f35a9fbc85275fa73
Stuart Grieve <s.grieve@qmul.ac.uk>
2021-07-27 15:18:56 +0100

    minor tweaks to the hack sinuosity code

--- plotting/grl-figs/sinuosity/hack_sinuosity.py
+++ plotting/grl-figs/sinuosity/hack_sinuosity.py
@@ -9,15 +9,14 @@ euc = []
 areas = []
 flowlens = []
 
-for i, z in enumerate(zones):
+for z in zones:
     files = glob('../../../Results/{0}*/{0}*_river*.csv'.format(z))
-    files = files[::30]
+    files = files[::25]
     print(len(files))
     for filename in files:
         data = np.genfromtxt(filename, delimiter=',')
 
         flow_length = np.max(np.abs(data[:, 5] - np.max(data[:, 5])))
-
         area = np.max(data[:, 6])
 
         top = (data[0][0], data[0][1])
@@ -35,7 +34,6 @@ for i, lenth_option in enumerate([euc, flowlens]):
     x = np.log(areas)
     y = np.log(lenth_option)
 
-
     slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
     print(slope)
 
@@ -48,11 +46,11 @@ for i, lenth_option in enumerate([euc, flowlens]):
     plt.ylabel('Length ($km$)')
 
     axes = plt.gca()
-    a=axes.get_xticks().tolist()
+    a = axes.get_xticks().tolist()
     a = [int(round(np.power(np.e, q)/1000000, 0)) for q in a]
     axes.set_xticklabels(a)
 
-    a=axes.get_yticks().tolist()
+    a = axes.get_yticks().tolist()
     a = [int(round(np.power(np.e, q)/1000, 0)) for q in a]
     axes.set_yticklabels(a)
 
