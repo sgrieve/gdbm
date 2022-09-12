dee431f225d4f270b76c5c51c901696d09627329
Stuart Grieve <s.grieve@qmul.ac.uk>
2022-09-12 19:52:23 +0100

    pit filling histogram code

---
+++ plotting/data-paper-figs/pit-fill-fig.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+data = '../../dems/11_0_deltas.csv'
+
+plt.rcParams['font.family'] = 'sans-serif'
+
+deltas = pd.read_csv(data)
+
+pc = np.percentile(deltas['d'], 98)
+
+plt.hist(deltas['d'], bins=20, density=False, edgecolor='k', linewidth=0.5)
+max_count = plt.gca().get_ylim()[1] + 100000  # Constant is to create headroom at top of plot
+
+plt.vlines(pc, 0, max_count, linewidth=1, color='k', edgecolor='k', linestyle='--',label='98th percentile')
+
+plt.xlabel('Vertical change (m)')
+plt.ylabel('Density')
+
+plt.yscale('log')
+plt.xlim(0, 18)
+plt.ylim(ymax=max_count)
+
+plt.errorbar(x=7.25, y=100000, xerr=2.25, c='k', linewidth=0.5, capsize=4,
+             capthick=0.5)
+
+# Dummy data to make a cleaner legend
+plt.plot([7.25, 7.8], [100000, 100000], c='k', linewidth=0.5,
+         label='SRTM vertical error range')
+
+plt.legend()
+
+plt.tight_layout()
+
+
+# plt.savefig('pit-fill.png')
+plt.savefig('/Users/stuart/Papers/gdbm-data-paper/figs/pit-fill.pdf')
